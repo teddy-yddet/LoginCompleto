fc605a940f66e2ee17138e00f7edcd9edd71cce7
sidne <sidnei.m@aluno.ifsp.edu.br>
2023-11-12 09:56:07 -0300

    logincompleto

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'loginauth';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    IF($conexao->connect_errno)
+    {
+        echo "Erro";
+    }
+    else
+    {
+        echo "Conexão efetuada com sucesso!";
+    }
+?>
