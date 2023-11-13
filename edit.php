e17ee1249ddfd5f50ff07b4f6c2f3858e3921ec2
Sidnei <sidnei.m@aluno.ifsp.edu.br>
2023-11-13 11:56:26 -0300

    eliminado excessos

--- edit.php
+++ edit.php
@@ -37,7 +37,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário | GN</title>
+    <title>Formulário</title>
     <style>
         body{
             font-family: Arial, Helvetica, sans-serif;
@@ -117,7 +117,7 @@
     <div class="box">
         <form action="saveEdit.php" method="POST">
             <fieldset>
-                <legend><b>Editar Cliente</b></legend>
+                <legend><b>Editar</b></legend>
                 <br>
                 <div class="inputBox">
                     <input type="text" name="nome" id="nome" class="inputUser" value=<?php echo $nome;?> required>
